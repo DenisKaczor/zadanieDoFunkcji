c6351b68ce0f1e310dfbb7842f907c28bebb928b
DenisKaczor <chx100210@student.chorzow.wsb.pl>
2023-06-27 21:42:50 +0200

    v1

---
+++ zadanie1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("podaj czas w sekundach");
+			double czas = int.Parse(Console.ReadLine());
+			double g = godzina(czas);
+			double m = minuta(czas);
+			double s = sekunda(czas);
+			string timeString = g.ToString("00") + ":" + m.ToString("00") + ":" + s.ToString("00");
+			Console.WriteLine(timeString);
+			Console.ReadLine();
+		}
+		static double godzina(double czas)
+		{
+			double godziny=0;
+			godziny = czas / 3600;
+			godziny = Math.Floor(godziny);
+			return godziny;
+		}
+		static double minuta(double czas)
+		{
+			double minuty;
+			minuty = czas/60;
+			minuty -= godzina(czas) * 60;
+			minuty = Math.Floor(minuty);
+			return minuty;
+		}
+		static double sekunda(double czas)
+		{
+			double sekundy;
+			sekundy = czas/60 - Math.Floor(czas/60);
+			sekundy *= 0.6;
+			sekundy *= 100;
+			sekundy =Math.Round(sekundy);
+			return sekundy;
+		}
+
+	}
+}
---
+++ zadanie2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie2
+{
+	internal class Program
+	{
+		static double ZnajdzWieksza(double a, double b)
+		{
+
+			double dlugosc;
+			if (a > b)
+			{
+				dlugosc = a - b;
+			}
+			else
+			{
+				dlugosc = b - a;
+			}
+			return dlugosc;
+		}
+		static double ObliczDlugoscOdcinka(double x1, double x2, double y1, double y2)
+		{
+			double dlugosc;
+			if (x1==x2)
+			{
+				dlugosc = ZnajdzWieksza(y1, y2);
+			}
+			else if (y1==y2)
+			{
+				dlugosc = ZnajdzWieksza(x1, x2);
+			}
+			else
+			{
+				double x = ZnajdzWieksza(x1, x2);
+				double y = ZnajdzWieksza(y1, y2);
+				x = Math.Pow(x,2);
+				y = Math.Pow(y,2);
+				dlugosc = Math.Sqrt(x+y);
+			}
+			return dlugosc;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("podaj parametr x pierwszego punktu");
+			double x1  = double.Parse(Console.ReadLine());
+			Console.WriteLine("podaj parametr x drugiego punktu");
+			double x2 = double.Parse(Console.ReadLine());
+			Console.WriteLine("podaj parametr y pierwszego punktu");
+			double y1 = double.Parse(Console.ReadLine());
+			Console.WriteLine("podaj parametr y drugiego punktu");
+			double y2 = double.Parse(Console.ReadLine());
+			double dlugosc = ObliczDlugoscOdcinka(x1, x2, y1, y2);
+			/////////////////////////////////////////////
+			Console.WriteLine("Długość twojego odcinka wynosi: " + dlugosc);
+			Console.ReadLine();
+		}
+
+	}
+}
